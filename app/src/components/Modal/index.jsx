aee7cee8deb54b37aa286ba8807a6592f3ad2178
JBC <hello@jeanbaptistecalzia.fr>
2025-02-07 20:53:53 +0100

    Set prop types for Modal component

--- app/src/components/Modal/index.jsx
+++ app/src/components/Modal/index.jsx
@@ -58,7 +58,7 @@ function Modal({
 }
 
 Modal.propTypes = {
-  isShow: PropTypes.bool.isRequired,
+  isShow: PropTypes.bool,
   isClose: PropTypes.func.isRequired,
   isFading: PropTypes.bool,
   isSlideIn: PropTypes.bool,
