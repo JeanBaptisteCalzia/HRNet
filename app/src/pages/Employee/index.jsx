ea50d447d22b1cd564c690e36e4b971680a0ea8f
JBC <hello@jeanbaptistecalzia.fr>
2025-02-11 09:11:52 +0100

    Add propTypes for FilterComponent

--- app/src/pages/Employee/index.jsx
+++ app/src/pages/Employee/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState, useMemo, useEffect } from "react";
 // import Table from "../../components/Table/"; // Custom component
 import TopBar from "../../components/TopBar/";
@@ -194,5 +195,11 @@ function Employee() {
     </>
   );
 }
+
+FilterComponent.propTypes = {
+  filterText: PropTypes.string,
+  onFilter: PropTypes.func,
+  onClear: PropTypes.func,
+};
 
 export default Employee;
